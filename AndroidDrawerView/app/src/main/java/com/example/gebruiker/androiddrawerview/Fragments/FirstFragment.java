9be0969d61659a48953cc0b70a7053e19273aec1
Wartenberg,Koen K.W <k.wartenberg@student.fontys.nl>
2017-06-02 13:19:40 +0200

    android drawer

---
+++ AndroidDrawerView/app/src/main/java/com/example/gebruiker/androiddrawerview/Fragments/FirstFragment.java
@@ -0,0 +1,24 @@
+package com.example.gebruiker.androiddrawerview.Fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.gebruiker.androiddrawerview.R;
+
+/**
+ * Created by user on 12/31/15.
+ */
+public class FirstFragment extends Fragment {
+
+    View myView;
+
+    //@Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.first_layout, container, false);
+        return myView;
+    }
+}
