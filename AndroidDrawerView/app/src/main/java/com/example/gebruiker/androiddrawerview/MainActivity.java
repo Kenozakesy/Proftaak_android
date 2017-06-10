834e6f6d0b0bd3a1ceac046cafa929eebea5d17e
Tran,Hanh T.D.H <h.tran@student.fontys.nl>
2017-06-10 17:09:51 +0200

    loginActivity

--- AndroidDrawerView/app/src/main/java/com/example/gebruiker/androiddrawerview/MainActivity.java
+++ AndroidDrawerView/app/src/main/java/com/example/gebruiker/androiddrawerview/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.gebruiker.androiddrawerview;
 
-import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -11,12 +10,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.example.gebruiker.androiddrawerview.Fragments.FirstFragment;
-import com.example.gebruiker.androiddrawerview.Fragments.SecondFragment;
-import com.example.gebruiker.androiddrawerview.Fragments.ThirdFragment;
-
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +26,6 @@ public class MainActivity extends AppCompatActivity
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
@@ -67,56 +60,12 @@ public class MainActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-        FragmentManager fragmentManager = getFragmentManager();
+    public static class HomeActivity extends AppCompatActivity {
 
-        if (id == R.id.nav_Home) {
-            fragmentManager.beginTransaction()
-                    .replace(R.id.content_frame
-                            , new FirstFragment())//here comes new link)
-                    .commit();
-        } else if (id == R.id.nav_choose_a_category) {
-            fragmentManager.beginTransaction()
-                    .replace(R.id.content_frame
-                            , new SecondFragment())
-                    .commit();
-        } else if (id == R.id.nav_orders) {
-            fragmentManager.beginTransaction()
-                    .replace(R.id.content_frame
-                            , new ThirdFragment())
-                    .commit();
-        } else if (id == R.id.nav_facture) {
-            //do something here
-        } else if (id == R.id.nav_Ebook_library) {
-
-        } else if (id == R.id.nav_Account_options) {
-
-        } else if (id == R.id.nav_item_settings) {
-
-        } else if (id == R.id.nav_blog) {
-
-        } else if (id == R.id.nav_customer_service) {
-
-        } else if (id == R.id.nav_order_safe) {
-
-        } else if (id == R.id.nav_privacy) {
-
-        } else if (id == R.id.nav_Standardprocedures) {
-
-        } else if (id == R.id.nav_options) {
-
-        } else if (id == R.id.nav_feedback) {
-
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_home);
         }
-
-
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
     }
 }
