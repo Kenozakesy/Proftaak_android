e5849c9863e9e87679a2a976466f7c67287f0b5a
Tran,Hanh T.D.H <h.tran@student.fontys.nl>
2017-06-22 10:34:38 +0200

    fix color

--- AndroidDrawerView/app/src/main/java/com/example/gebruiker/androiddrawerview/MainActivity.java
+++ AndroidDrawerView/app/src/main/java/com/example/gebruiker/androiddrawerview/MainActivity.java
@@ -49,11 +49,15 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        Button button =(Button) findViewById(R.id.buttonKlantenservice);
+        button.setBackgroundColor(getResources().getColor(R.color.buttonColor));
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
+
 
         getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
 
